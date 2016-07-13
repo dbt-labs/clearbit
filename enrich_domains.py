a6aef7fdfa0d9626c9b7d031f066d9949768b7be
Drew Banin <drew@8tracks.com>
2016-07-13 17:31:22 -0400

    log output and error handling

--- enrich_domains.py
+++ enrich_domains.py
@@ -2,8 +2,8 @@ import requests
 import clearbit
 import time
 import yaml
+import sys
 import fetch_data
-
 
 def format_request(company):
     company['response_timestamp'] = time.time()
@@ -56,9 +56,24 @@ def fetch_and_process(query):
     batch_size = fetch_config['batch_size']
 
     request_list = []
+    counts = {
+        'total': 0,
+        'ok': 0,
+        'error': 0,
+    }
     def handle_row(row):
+        counts['total'] += 1
+
         domain = row[0]
-        company = get_company(domain)
+
+        try:
+          company = get_company(domain)
+        except requests.exceptions.RequestException as e:
+          counts['error'] += 1
+          print "ERROR: {}".format(e.message)
+          return
+
+        counts['ok'] += 1
         if company:
             data = format_request(company)
         else:
@@ -69,7 +84,21 @@ def fetch_and_process(query):
             send_to_rj(request_list)
             del request_list[:]
 
+        if counts['total'] % 2 == 0:
+          print "TOTAL: {}\tOK: {}\tERROR: {}".format(counts['total'], counts['ok'], counts['error'])
+
+        if counts['error'] > int(counts['total'] * 0.1) and counts['total'] >= 10:
+          print "TOO MANY ERRORS (> 10%) -- Quitting!"
+          sys.exit(1)
+
     fetch_data.fetch(fetch_config, query, handle_row)
+
+    # clean up any leftovers
+    if len(request_list) > 0:
+      send_to_rj(request_list)
+
+    print "DONE"
+    print "TOTAL: {}\tOK: {}\tERROR: {}".format(counts['total'], counts['ok'], counts['error'])
 
 def process_list(domains):
     data = []
